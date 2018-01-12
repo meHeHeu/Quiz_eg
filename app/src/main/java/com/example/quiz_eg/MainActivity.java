a1b8eca69a9fcd256a1a40244d36d3dee080a563
meheheu <meHeHeu@gmail.com>
2018-01-12 09:11:47 +0100

    a

--- app/src/main/java/com/example/quiz_eg/MainActivity.java
+++ app/src/main/java/com/example/quiz_eg/MainActivity.java
@@ -5,19 +5,55 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button answer1Button,
-           answer2Button,
-           answer3Button,
-           answer4Button;
+    Questions questions;
 
-    TextView questionTextView,
-             scoreTextView;
+    Random random;
+
+    @BindView(R.id.answer1Button)
+    Button answer1Button;
+    @BindView(R.id.answer2Button)
+    Button answer2Button;
+    @BindView(R.id.answer3Button)
+    Button answer3Button;
+    @BindView(R.id.answer4Button)
+    Button answer4Button;
+
+    @BindView(R.id.questionTextView)
+    TextView questionTextView;
+    @BindView(R.id.scoreTextView)
+    TextView scoreTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        questions = new Questions();
+
+        random = new Random();
+
+        ButterKnife.bind(this);
+        scoreTextView.setText(getString(R.string.scoreTextView, 0));
+
+        updateQuestion();
+    }
+
+    @OnClick(R.id.answer1Button)
+    public void answer1Button_onClick(Button b) {
+
+    }
+
+    void updateQuestion() {
+        Question question = questions.getQuestion(0);
+
+        questionTextView.setText(question.body);
     }
 }
--- app/src/main/java/com/example/quiz_eg/Questions.java
+++ app/src/main/java/com/example/quiz_eg/Questions.java
@@ -1,17 +1,32 @@
 package com.example.quiz_eg;
 
-public class Questions {
-    class Question {
-        final String body;
-        final String answer;
-        final String[] badAnswer;
-        Question (String body, String answer, String... badAnswer) {
-            this.body = body;
-            this.answer = answer;
-            this.badAnswer = badAnswer;
-        }
-    };
-    Question[] question;
+class Question {
 
+    public final String body;
+    public final String answer;
+    public final String[] badAnswer;
 
+    Question (String body, String answer, String... badAnswer) {
+        this.body = body;
+        this.answer = answer;
+        this.badAnswer = badAnswer;
+    }
 }
+
+class Questions {
+
+    private Question[] question;
+
+    public Questions() {
+        question = new Question[3];
+        question[0] = new Question("Czy pierniki są słodkie?", "Yes", "No", "I dunno", "Yes and no");
+        question[1] = new Question("Czy ziemniaki są słodkie?", "Yes", "No", "I dunno", "Yes and no");
+        question[2] = new Question("Czy bułeczki są słodkie?", "Yes", "No", "I dunno", "Yes and no");
+    }
+
+    public Question getQuestion(int i) {
+        return question[i];
+    }
+}
+
+
