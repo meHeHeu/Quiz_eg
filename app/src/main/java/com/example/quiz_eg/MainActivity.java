6550750ee32acdd5c29a83e8725baac2c9ec7b29
meheheu <meHeHeu@gmail.com>
2018-01-15 11:08:08 +0100

    Add parser for xml files with questions.

--- app/src/main/java/com/example/quiz_eg/MainActivity.java
+++ app/src/main/java/com/example/quiz_eg/MainActivity.java
@@ -1,30 +1,39 @@
 package com.example.quiz_eg;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Random;
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int STORAGE_PERMISSION_CODE = 1;
+
     Questions questions;
+    Questions.Question currentQuestion;
+    int currentQuestionIndex;
 
-    Random random;
+    int score;
 
-    @BindView(R.id.answer1Button)
-    Button answer1Button;
-    @BindView(R.id.answer2Button)
-    Button answer2Button;
-    @BindView(R.id.answer3Button)
-    Button answer3Button;
-    @BindView(R.id.answer4Button)
-    Button answer4Button;
+    @BindViews({R.id.answer1Button, R.id.answer2Button, R.id.answer3Button, R.id.answer4Button})
+    List<Button> answerButtons;
 
     @BindView(R.id.questionTextView)
     TextView questionTextView;
@@ -36,24 +45,110 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        questions = new Questions();
-
-        random = new Random();
-
         ButterKnife.bind(this);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            requestStoragePermission();
+
         scoreTextView.setText(getString(R.string.scoreTextView, 0));
+
+        String[] quizQuestionsPath = new String[]{
+                Environment.getExternalStorageDirectory().getPath()+"/"+getString(R.string.app_name),
+                Environment.getExternalStorageDirectory().getPath()+"/Android/data/"+getString(R.string.app_name),
+                Environment.getExternalStorageDirectory().getPath()+"/Documents/data/"+getString(R.string.app_name)
+        };
+
+        questions = new Questions(quizQuestionsPath);
+        currentQuestionIndex = 0;
+
+        score = 0;
 
         updateQuestion();
     }
 
-    @OnClick(R.id.answer1Button)
-    public void answer1Button_onClick(Button b) {
-
+    @OnClick({R.id.answer1Button, R.id.answer2Button, R.id.answer3Button, R.id.answer4Button})
+    public void answerButton_onClick(Button b) {
+        if(b.getText().equals(currentQuestion.answer)) {
+            ++score;
+            scoreTextView.setText(getString(R.string.scoreTextView, score));
+            Toast.makeText(this, R.string.goodAnswer, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, R.string.badAnswer, Toast.LENGTH_SHORT).show();
+        }
+        if((++currentQuestionIndex)<questions.amount())
+            updateQuestion();
+        else
+            gameOver();
     }
 
-    void updateQuestion() {
-        Question question = questions.getQuestion(0);
+    private void updateQuestion() {
+        currentQuestion = questions.getQuestion(currentQuestionIndex);
 
-        questionTextView.setText(question.body);
+        questionTextView.setText(currentQuestion.body);
+
+        String[] answers = new String[4];
+        answers[0] = currentQuestion.answer;
+        for(int i=1; i<4; ++i)
+            answers[i] = currentQuestion.badAnswer[i-1];
+        MyUtils.<String>ShuffleArray(answers);
+
+        for(int i=0; i<4; ++i)
+            answerButtons.get(i).setText(answers[i]);
+    }
+
+    private void gameOver() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setMessage(getString(R.string.gameOverMsg, score)).setCancelable(false).setPositiveButton(R.string.tryAgainLabel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        })
+                .setNegativeButton(R.string.exitLabel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+
+                    }
+                });
+        AlertDialog alertDialog=alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.permissionReqTitle)
+                    .setMessage(R.string.permissionReqMsg)
+                    .setPositiveButton(R.string.okLabel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton(R.string.cancelLabel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            finish();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == STORAGE_PERMISSION_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                finish();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/quiz_eg/Questions.java
+++ app/src/main/java/com/example/quiz_eg/Questions.java
@@ -1,32 +1,206 @@
 package com.example.quiz_eg;
 
-class Question {
+import android.util.Log;
+import android.util.Xml;
 
-    public final String body;
-    public final String answer;
-    public final String[] badAnswer;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
-    Question (String body, String answer, String... badAnswer) {
-        this.body = body;
-        this.answer = answer;
-        this.badAnswer = badAnswer;
-    }
-}
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 class Questions {
 
     private Question[] question;
 
-    public Questions() {
-        question = new Question[3];
-        question[0] = new Question("Czy pierniki są słodkie?", "Yes", "No", "I dunno", "Yes and no");
-        question[1] = new Question("Czy ziemniaki są słodkie?", "Yes", "No", "I dunno", "Yes and no");
-        question[2] = new Question("Czy bułeczki są słodkie?", "Yes", "No", "I dunno", "Yes and no");
+    class Question {
+
+        public final String body;
+        public final String answer;
+        public final String[] badAnswer;
+
+        Question (String body, String answer, String... badAnswer) {
+            this.body = body;
+            this.answer = answer;
+            this.badAnswer = badAnswer;
+        }
+    }
+    /*
+
+    :O
+    https://developer.android.com/training/basics/network-ops/xml.html
+    Zrobiłem to WŁAŚNIE W TEN SPOSÓB zanim jeszcze zacząłem opracowywać parser.
+    (wewnętrzna klasa, pola publiczne, finalne; konstruktor)
+    Jestem zajebisty.
+
+     */
+
+    class TestXmlParser {
+
+        private final String ns = null;
+
+        public List<Question> parse(FileInputStream fileInputStreams)
+                throws XmlPullParserException, IOException {
+            try {
+                XmlPullParser parser = Xml.newPullParser();
+                parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                parser.setInput(fileInputStreams, null);
+                parser.nextTag();
+                return readTest(parser);
+            } finally {
+                fileInputStreams.close();
+            }
+        }
+
+        private List<Question> readTest(XmlPullParser parser)
+                throws XmlPullParserException, IOException {
+
+            ArrayList<Question> result = new ArrayList<Question>();
+
+            parser.require(XmlPullParser.START_TAG, ns, "test");
+
+            while(parser.next() != XmlPullParser.END_TAG) {
+
+                if(parser.getEventType() != XmlPullParser.START_TAG)
+                    continue;
+
+                String name = parser.getName();
+                if(name.equals("question"))
+                    result.add(readQuestion(parser));
+                else
+                    skip(parser);
+            }
+
+            return result;
+        }
+
+        private Question readQuestion(XmlPullParser parser)
+                throws XmlPullParserException, IOException {
+
+            String body = null;
+            String answer = null;
+            String[] banswer = new String[3];
+            int banswer_num = 0;
+
+            parser.require(XmlPullParser.START_TAG, ns, "question");
+
+            while(parser.next() != XmlPullParser.END_TAG) {
+
+                if(parser.getEventType() != XmlPullParser.START_TAG)
+                    continue;
+
+                String name = parser.getName();
+                if(name.equals("banswer"))
+                    banswer[banswer_num++] = readEntry(parser, "banswer"); // FIXME: if more entries of banswer found in a xml file, this will crash an app (or cause undefined behaviour)
+                else if(name.equals("answer"))
+                    answer = readEntry(parser, "answer");
+                else if(name.equals("body"))
+                    body = readEntry(parser, "body");
+                else
+                    skip(parser);
+            }
+
+            return new Question(body, answer, banswer);
+        }
+
+        private String readEntry(XmlPullParser parser, String entryName)
+                throws XmlPullParserException, IOException {
+
+            parser.require(XmlPullParser.START_TAG, ns, entryName);
+            String result = readText(parser);
+            parser.require(XmlPullParser.END_TAG, ns, entryName);
+
+            return result;
+        }
+
+        private String readText(XmlPullParser parser)
+                throws XmlPullParserException, IOException {
+
+            String result = "";
+
+            if(parser.next() == XmlPullParser.TEXT) {
+                result = parser.getText();
+                parser.nextTag();
+            }
+
+            return result;
+        }
+
+        private void skip(XmlPullParser parser)
+                throws XmlPullParserException, IOException {
+
+            if(parser.getEventType() != XmlPullParser.START_TAG)
+                throw new IllegalStateException();
+
+            int depth = 1;
+            while(depth != 0) {
+                switch(parser.next()) {
+                    case XmlPullParser.END_TAG:
+                        --depth;
+                        break;
+                    case XmlPullParser.START_TAG:
+                        ++depth;
+                        break;
+                }
+            }
+        }
+
+        public void ExampleParsing()
+                throws XmlPullParserException, IOException {
+            XmlPullParser xpp = Xml.newPullParser();
+
+            xpp.setInput( new StringReader( "<foo>Hello World!</foo>" ) );
+            int eventType = xpp.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if(eventType == XmlPullParser.START_DOCUMENT) {
+                    Log.i("ExampleParsing", "Start document");
+                } else if(eventType == XmlPullParser.START_TAG) {
+                    Log.i("ExampleParsing", "Start tag "+xpp.getName());
+                } else if(eventType == XmlPullParser.END_TAG) {
+                    Log.i("ExampleParsing", "End tag "+xpp.getName());
+                } else if(eventType == XmlPullParser.TEXT) {
+                    Log.i("ExampleParsing", "Text "+xpp.getText());
+                }
+                eventType = xpp.next();
+            }
+            Log.i("ExampleParsing", "End document");
+        }
     }
 
-    public Question getQuestion(int i) {
+    Questions(String ...fileNames) {
+
+        List<File> quizFiles = new ArrayList<File>();
+        List<Question> quizQuestions = new ArrayList<Question>();
+        TestXmlParser testXmlParser = new TestXmlParser();
+
+        for(String fileName : fileNames)
+            quizFiles.addAll(MyUtils.FileListByExtension(new File(fileName), new String[]{".xml"}));
+
+        for(File file : quizFiles)
+            try {
+                quizQuestions.addAll(testXmlParser.parse(new FileInputStream(file)));
+            } catch(IOException e) {
+                e.printStackTrace();
+                continue;
+            } catch(XmlPullParserException e) {
+                e.printStackTrace();
+                continue;
+            }
+
+        question = quizQuestions.toArray(new Question[quizQuestions.size()]);
+
+        MyUtils.<Question>ShuffleArray(question);
+    }
+
+    Question getQuestion(int i) {
         return question[i];
     }
+
+    int amount() {
+        return question.length;
+    }
 }
-
-
